abf8f925cf81f7389a173ef06403ae723530a28f
MaximeBattu <m.battu74@gmail.com>
2023-01-31 15:09:55 +0100

    runners

--- api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
+++ api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleApiApplication {
 
-// Incroyable
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApiApplication.class, args);
 	}
